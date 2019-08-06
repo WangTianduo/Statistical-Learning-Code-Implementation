de5644926a38b86e18500493a5478fdbadb99588
Tianduo Wang <wtianduo@gmail.com>
2019-08-06 18:30:05 +0800

    no bug

--- 5.decision_tree/decision_tree.py
+++ 5.decision_tree/decision_tree.py
@@ -66,6 +66,9 @@ def get_best_feature(D_x, D_y):
     '''
 
     # discrete_x = discretize(D_x, 10)
+    D_x = np.array(D_x)
+    D_y = np.array(D_y).T
+
     discrete_x = D_x
 
     feature_num = D_x.shape[1]
@@ -74,22 +77,142 @@ def get_best_feature(D_x, D_y):
     max_feature = -1
 
     H_D = cal_shanno_ent(D_y)
-    print(H_D)
+    # print(H_D)
     for feature in range(feature_num):
 
         temp = np.array(discrete_x[:, feature].flat)
         G_D_A = H_D - cal_H_D_A(temp, D_y)
-        print(G_D_A, feature)
+        # print(G_D_A, feature)
         if G_D_A > max_G_D_A:
             max_G_D_A = G_D_A
             max_feature = feature
     return max_feature, max_G_D_A
 
 
-cancer_set_info = get_dataset('breast_cancer')
+def majorClass(labelArr):
+    '''
+    找到当前标签集中占数目最大的标签
+    :param labelArr: 标签集
+    :return: 最大的标签
+    '''
+    #建立字典，用于不同类别的标签技术
+    classDict = {}
+    #遍历所有标签
+    for i in range(len(labelArr)):
+        #当第一次遇到A标签时，字典内还没有A标签，这时候直接幅值加1是错误的，
+        #所以需要判断字典中是否有该键，没有则创建，有就直接自增
+        if labelArr[i] in classDict.keys():
+            # 若在字典中存在该标签，则直接加1
+            classDict[labelArr[i]] += 1
+        else:
+            #若无该标签，设初值为1，表示出现了1次了
+            classDict[labelArr[i]] = 1
+    #对字典依据值进行降序排序
+    classSort = sorted(classDict.items(), key=lambda x: x[1], reverse=True)
+    #返回最大一项的标签，即占数目最多的标签
+    return classSort[0][0]
 
-X_train, X_test, Y_train, Y_test = cancer_set_info.split()
 
-print(get_best_feature(X_train,  Y_train))
+def get_subdata_array(train_x, train_y, A, a):
+    '''
+    update dataset
+    :param train_x, train_y: dataset
+    :param A: the index of the eliminting feature
+    :param a: if data[A] == a, then keep the line
+    :return: new dataset
+    '''
+    retDataArr = []
+    retLabelArr = []
+
+    for i in range(len(train_x)):
+        if train_x[i][A] == a:
+            retDataArr.append(np.concatenate([train_x[i][0:A],train_x[i][A+1:]]))
+            retLabelArr.append(train_y[i])
+
+    return retDataArr, retLabelArr
+
+
+def create_tree(*dataSet):
+    '''
+    recursively create decision tree
+    :param dataSet: (train_x, train_y)
+    :return: ??
+    '''
+
+    Epsilon = 0.1
+
+    train_x = dataSet[0][0]
+    train_y = dataSet[0][1]
+
+    # print('train_x shape:{}'.format(train_x.shape))
+    # print('train_y shape:{}'.format(train_y.shape))
+    print('start a node: ', len(train_x[0]), len(train_y))
+
+    classDict = {i for i in train_y}
+
+    if len(classDict) == 0:
+        return majorClass(train_y)
+    elif len(classDict) == 1:
+        return train_y[0]
+    else:
+        Ag, EpsilonGet = get_best_feature(train_x, train_y)
+
+        if EpsilonGet < Epsilon:
+            return majorClass(train_y)
+
+        treeDict = {Ag:{}}
+
+        treeDict[Ag][0] = create_tree(get_subdata_array(train_x, train_y, Ag, 0))
+        treeDict[Ag][1] = create_tree(get_subdata_array(train_x, train_y, Ag, 1))
+
+        return treeDict
+
+
+def predict(test_x, tree):
+    # test_x = test_x.tolist()
+    while True:
+        (key, value), = tree.items()
+
+        if type(tree[key]).__name__ == 'dict':
+            dataVal = test_x[key]
+
+            print(test_x)
+            test_x = np.concatenate([test_x[0:key],test_x[key+1:]])
+
+            tree = value[dataVal]
+            print(tree)
+
+            if type(tree).__name__ != 'dict':
+                return tree
+        else:
+            return value
+
+def test(test_x, test_y, tree):
+    
+    error_cnt = 0
+    for i in range(len(test_x)):
+
+        if test_y[i] != predict(test_x[i], tree):
+            error_cnt += 1
+
+    return 1 - error_cnt / len(test_x)
+
+
+if __name__ == '__main__':
+    start = time.time()
+
+    cancer_set_info = get_dataset('breast_cancer')
+    train_x, test_x, train_y, test_y = cancer_set_info.split()
+
+    train_x = discretize(train_x, 2)
+    tree = create_tree((train_x, train_y))
+
+    print('tree:{}'.format(tree))
+    test_x = discretize(test_x, 2)
+    acc = test(test_x, test_y, tree)
+    print('accuracy is {}'.format(acc))
+
+    end = time.time()
+    print('time spent:{}'.format(end-start))
 
 
